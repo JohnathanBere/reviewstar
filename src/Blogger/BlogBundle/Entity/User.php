775f0992367a1adf0cb06d027646b4462f7f3464
JohnathanBere <johnbere@live.co.uk>
2017-10-25 14:26:06 +0100

    Added an entity and a repository, got rid of the yaml entity.

---
+++ src/Blogger/BlogBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Blogger\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="ws_users")
+ * @ORM\Entity(repositoryClass="Blogger\BlogBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
